650ed64655c8ae494a26484b1b5cbe88cc7fa8a7
Ser-An-Pol <seranpol@icloud.com>
2024-05-23 18:31:31 +0300

    Create method for print squares. lesson3:Task2

---
+++ lesson3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquares(int limit)
+{
+    int i = 0;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
