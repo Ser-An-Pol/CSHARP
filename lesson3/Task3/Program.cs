b10fe82e5bc6b7b8f85a976082fa40dbf7c055e4
Ser-An-Pol <seranpol@icloud.com>
2024-05-23 19:30:17 +0300

    Add methods with parameters passed by reference. lesson3:Task3

---
+++ lesson3/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) arr[i] = 0;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] arr)
+{
+    Random rnd = new();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 1000);
+    }
+}
+
+Console.WriteLine("Enter length of array...");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+
+FillArray(array);
+PrintArray(array);
+ZeroEvenElements(array);
+PrintArray(array);
