59254572ce1b2b95b63560fe79aa0a5d7c4df4fc
Ser-An-Pol <seranpol@icloud.com>
2024-05-23 13:12:56 +0300

    Find maximum from 5 numbers in lesson1:Task4

---
+++ lesson1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 5, b = 12, c = 6, d = 9, e = 8;
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+if (c > max) 
+{
+    max = b;
+}
+if (d > max) 
+{
+    max = b;
+}
+if (e > max) 
+{
+    max = b;
+}
+
+Console.WriteLine(max);
