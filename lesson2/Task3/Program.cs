2db3410836deaaf8e93c99d3657941a1e768e1fe
Ser-An-Pol <seranpol@icloud.com>
2024-05-23 16:45:38 +0300

    Output even elements of array in lesson2:Task3

---
+++ lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter array's length...");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next()/10000000;
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if ((arr[i] % 2) == 0) Console.Write($"{arr[i]} ");
+}
