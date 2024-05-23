6dd5d54a31310916f4a0e6df60a90452331ef48a
Ser-An-Pol <seranpol@icloud.com>
2024-05-23 15:56:47 +0300

    Full and output array in lesson2:Task2

---
+++ lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+Console.WriteLine("Enter length of array:");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.Write(arr[i] + " ");
+    Console.Write($"{arr[i]} ");
+    i++;
+}
