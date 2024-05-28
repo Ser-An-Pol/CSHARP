4be1513c965af9a4a1ee267e4f74ef918a0c32c7
Ser-An-Pol <seranpol@icloud.com>
2024-05-28 18:21:53 +0300

    Calculation of factorial: lesson5:Task1

---
+++ lesson5/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №1
+// • Вычислить факториал от натурального
+//   числа N
+
+long factorial(long num) {
+    if (num == 1 || num == 0) return 1;
+    //else return num*factorial(num-1);
+    else
+    {
+        long f = num*factorial(num-1);
+        System.Console.WriteLine($"Возврат: n = {num}, factorial = {f}");
+        return f;
+    }
+}
+
+System.Console.WriteLine(factorial(20));
