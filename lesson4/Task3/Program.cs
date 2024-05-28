267e5b1f57189433aeed89fe16ee9fc179d739e3
Ser-An-Pol <seranpol@icloud.com>
2024-05-28 14:57:38 +0300

    Delete any symbol from the string except letters: lesson4:Task3

---
+++ lesson4/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №3
+
+// Считать с консоли строку, состоящую из
+// цифр и латинских букв.
+
+// Сформировать новую строку, состоящую из
+// букв исходной строки
+
+// bool isLetter(char ch)
+// {
+//     if (ch >= 'A' && ch <= 'z') 
+//         return true;
+//     else
+//         return false; 
+// }
+
+string? str = System.Console.ReadLine();
+
+string res = "";
+
+foreach (char ch in str)
+    if (char.IsAsciiLetter(ch)) res += ch;
+
+System.Console.WriteLine(res);
