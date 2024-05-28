993c8ee403314e9d52d2c0809f6481a8a3028a7b
Ser-An-Pol <seranpol@icloud.com>
2024-05-28 13:25:30 +0300

    Create, fill and print matrix: lesson4:Task1

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №1
+// • Создать двумерный массив с размерами
+//    3 x 5, состоящий из целых чисел
+// • Вывести его элементы на экран
+
+
+
+int[,] CreateMatrix(int rowCount, int colCount)
+{
+    int[,] table = new int[rowCount, colCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return table;
+}
+
+void ShowMatrix(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            System.Console.Write($"{table[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
