acff70dabcb421954c768beedcef7d14a0e9d924
Ser-An-Pol <seranpol@icloud.com>
2024-06-05 15:00:29 +0300

    seminar5:task2 => Sum of elements of the main diagonal

---
+++ seminar5/task2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+
+Main();
+
+void Main()
+{
+    int rowSize = ReadInt("Enter count of row...");
+    int colSize = ReadInt("Enter count of column...");
+
+    int[,] array = CreateAndFill2dArray(rowSize, colSize, 0, 99);
+    Print2dArray(array);
+    System.Console.WriteLine($"Sum of elements of the main diagonal is {SumMainDiagonal(array)}");
+    
+
+}
+
+int ReadInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int num;
+    while (!int.TryParse(Console.ReadLine(), out num) || !(num > 0))
+    {
+        System.Console.WriteLine("It's not suitable number. Enter number again...");
+    }
+    return num;
+}
+
+int[,] CreateAndFill2dArray(int rowSize, int colSize, int minRange, int maxRange)
+{
+    int[,] array = new int[rowSize, colSize];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rowSize; i++)
+        for (int j = 0; j < colSize; j++)
+            array[i, j] = rnd.Next(minRange, maxRange + 1);
+
+    return array;
+}
+
+void Print2dArray(int[,] array)
+{
+    System.Console.WriteLine("***********");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($" {array[i, j], 4} ");
+        System.Console.WriteLine("]");
+    }
+    System.Console.WriteLine("***********");
+}
+
+int SumMainDiagonal(int[,] array)
+{
+    int length = (array.GetLength(0) > array.GetLength(1)) ? array.GetLength(1) : array.GetLength(0);
+    int sum = 0;
+
+    for (int i = 0; i < length; i++)
+        sum += array[i,i];
+    
+    return sum;
+}
