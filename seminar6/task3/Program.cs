87941928760e4dc73417f88fecc41d595185778e
Ser-An-Pol <seranpol@icloud.com>
2024-06-10 20:58:28 +0300

    seminar6:task3 => Count of vowels in string

---
+++ seminar6/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("Enter string...");
+    string? str = Console.ReadLine();
+
+    System.Console.WriteLine(CountVowels(str));
+    System.Console.WriteLine(str);
+}
+
+bool isVowel(char c)
+{
+    return "aeiouy".Contains(c);
+}
+
+int CountVowels(string? inStr)
+{
+    int count = 0;
+
+    string temp = inStr.ToLower();
+    foreach (char letter in temp)
+        if (isVowel(letter)) count++;
+    
+    return count;
+}
