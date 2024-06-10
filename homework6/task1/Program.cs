17697087ce0b0f33de0a8bd0b1cc95404798851a
Ser-An-Pol <seranpol@icloud.com>
2024-06-10 21:25:21 +0300

    homework6:task1 => Convert 2d-array of chars to string

---
+++ homework6/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+
+using System.Text;
+
+Main();
+
+void Main()
+{
+    char[,] arrChar = {{'a','b','c'},{'d','e','f'}};
+
+    System.Console.WriteLine(Convert2dArrayToString(arrChar));
+}
+
+string Convert2dArrayToString(char[,] chars)
+{
+    StringBuilder stringBuilder = new();
+
+    foreach (var item in chars)
+        stringBuilder.Append(item);
+
+    return stringBuilder.ToString();
+}
