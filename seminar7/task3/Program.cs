0bdb8c708600e569023df774307b1135c326f057
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 15:11:08 +0300

    seminar7:task3 => Print cosonants of string (recursion version)

---
+++ seminar7/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+string Consonants = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxZz";
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("Enter string...");
+    string? str = Console.ReadLine();
+    printIfConsonant(str!, str!.Length-1);
+}
+
+void printIfConsonant(string? str, int ind)
+{
+    if (ind >= str!.Length)
+    {
+        System.Console.WriteLine("Error...");
+        return;
+    }
+
+    if (ind < 0) return;
+    printIfConsonant(str!, ind-1);
+
+    if (Consonants.Contains(str![ind])) 
+        System.Console.Write($"{str![ind]} ");
+}
