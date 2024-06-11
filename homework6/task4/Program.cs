394495a263397cfc6332cf61aae5211a7e6af3fa
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 14:01:06 +0300

    homework6:task4 => Reverse words in sentence

---
+++ homework6/task4/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("Enter string...");
+    string? str = Console.ReadLine();
+
+    System.Console.WriteLine(reverseWords(str!));
+}
+
+string[] getWords(string? str)
+{
+    string[] words = str!.Split(" ");
+
+    return words;
+}
+
+string[] reverseArray(string[] words)
+{
+    string[] r_words = new string[words.Length];
+
+    for (int i = words.Length - 1, j = 0; i >= 0; i--, j++)
+        r_words[j] = words[i];
+
+    return r_words;
+}
+
+string getStringFromArray(string[] words)
+{
+    return string.Join(" ", words);
+}
+
+string reverseWords(string? str)
+{
+    return getStringFromArray(reverseArray(getWords(str!)));
+}
