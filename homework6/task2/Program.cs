489084d3c72bffcc54b3ef488270af772418bf10
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 13:36:52 +0300

    homework6:task2 => ToLower

---
+++ homework6/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+
+using System.Text;
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("Enter string...");
+    string? str = Console.ReadLine();
+
+    System.Console.WriteLine(myStrToLower(str));
+}
+
+char myToLower(char c)
+{
+    // string UP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // if (UP.Contains(c)) c = (char)((int)c + 32);
+    int c_ascii = (int)c;
+
+    if (c_ascii > 64 && c_ascii < 91) c_ascii += 32;
+
+    return (char)c_ascii;
+}
+
+string myStrToLower(string? str)
+{
+    StringBuilder sb = new();
+
+    for (int i = 0; i < str!.Length; i++)
+        sb.Append(myToLower(str[i]));
+
+    return sb.ToString();
+}
