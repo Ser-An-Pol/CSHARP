c05cf61af2a6f8e930c407b9c339a224ba7db7f5
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 13:48:15 +0300

    homework6:task3 => Is this string polyndrome?

---
+++ homework6/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+using System.Text;
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("Enter string...");
+    string? str = Console.ReadLine();
+
+    System.Console.WriteLine(isPolindrom(str!));
+}
+
+bool isPolindrom(string? str)
+{
+    if (str!.Equals(Reverse(str))) 
+        return true;
+    else
+        return false;
+}
+
+string Reverse(string? str)
+{
+    StringBuilder sb = new();
+
+    for (int i = str!.Length-1; i >= 0; i--)
+        sb.Append(str[i]);
+    
+    return sb.ToString();
+}
