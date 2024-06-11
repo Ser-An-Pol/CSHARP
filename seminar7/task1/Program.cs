a2971adbcaa962b751860a60cb2bb199029492a3
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 14:25:10 +0300

    seminar7:task1 => Sum of digits in number (recursionversion)

---
+++ seminar7/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+Main();
+
+void Main()
+{
+    int number = inputInt("Enter positive integer number...");
+
+    System.Console.WriteLine($"Sum of digits in {number} is {SumDigits(number)}");
+}
+
+int inputInt(string str)
+{
+    System.Console.WriteLine(str);
+    int num;
+
+    while (!int.TryParse(Console.ReadLine(), out num) || num < 0)
+        System.Console.WriteLine("Enter correct positive integer number...");
+
+    return num;
+}
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+
+    return num % 10 + SumDigits(num / 10);
+}
