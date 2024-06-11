fcb69abf09bd4fa3dd52ff2962e0f84a4c316f7b
Ser-An-Pol <seranpol@icloud.com>
2024-06-11 15:25:52 +0300

    homework7:task1 => Print all integer numbers from [M, N]

---
+++ homework7/task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+Main();
+
+void Main()
+{
+    int M = inputInt("Enter first integer number...");
+    int N = inputInt("Enter second integer number...");
+
+    printAllBetweenNumbers(M, N);
+}
+
+int inputInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int num;
+
+    while (!int.TryParse(Console.ReadLine(), out num))
+        System.Console.WriteLine("Enter correct integer number...");
+
+    return num;
+}
+
+void printAllBetweenNumbers(int begin, int end)
+{
+    if (begin > end)
+    {
+        int tmp = begin;
+        begin = end;
+        end = tmp;
+    }
+
+    if (begin == end)
+    {
+        System.Console.WriteLine(begin);
+        return;
+    }
+
+    System.Console.Write($"{begin}, ");
+    printAllBetweenNumbers(begin+1, end);
+}
+
